0678038d6a0173d91661aacf5cd898619907cc37
Zietieflr <lrmcguire93@gmail.com>
2020-07-07 20:57:49 -0600

    Add adaptive poseNet tracking. Refactor.

--- index.js
+++ index.js
@@ -13,53 +13,43 @@ const $ = {
   timer: document.querySelector('.timer')
 }
 
-const canvasRender = {
-  // active: beginDrawing(),
-  // inactive: endDrawing(),
-  // practice: practice(), 
+// const canvasRender = {
+//   active,
+//   inactive,
+//   practice, 
+// }
+
+function setup() {
+  // video height is 75% of width
+  createCanvas(640, 640).parent($.main)
+  video = createCapture(VIDEO)
+  video.hide()
+  frameRate(10)
+  poseNet = ml5.poseNet(video, modelLoaded)
+  poseNet.on('pose', capturePose)
 }
 
-// function setup() {
-//   // video height is 75% of width
-//   createCanvas(640, 640)
-//   video = createCapture(VIDEO)
-//   video.hide()
-//   frameRate(10)
-//   poseNet = ml5.poseNet(video, modelLoaded)
-//   poseNet.on('pose', capturePose)
-// }
-
-// function draw() {
-//   translate(video.width, 0)
-//   scale(-1, 1)
-//   image(video, 320, 640)
-//   background(250, 250, 250)
-
-//   if (pose) {
-//     fill(255, 0, 0);
-//     ellipse(pose.nose.x + 320, pose.nose.y, 25);
-//     drawPath.push(createVector(pose.nose.x + 320, pose.nose.y))
+function draw() {
+  translate(video.width, 0)
+  scale(-1, 1)
+  image(video, 320, 0)
+  // background(250, 250, 250)
 
 
-//     // fill(0, 255, 0)
-//     // ellipse(pose.leftWrist.x, pose.leftWrist.y, 25)
-//     // ellipse(pose.rightWrist.x, pose.rightWrist.y, 25)
+  bodyPointTracking('leftWrist', pose, drawPath)
 
-//     // drawLeftWrist(pose)
-//   }
-
-//   if(drawPath) {
-//     noFill() 
-//     strokeWeight(25)
-//     stroke(255, 0, 0)
-//     beginShape()
-//     drawPath.forEach(point => vertex(point.x, point.y))
-//     endShape()
-//     if(drawPath.length > 5) {
-//       drawPath.shift()
-//     }
-//   }
-// }
+  // if(drawPath) {
+  //   noFill() 
+  //   strokeWeight(25)
+  //   stroke(255, 0, 0)
+  //   beginShape()
+  //   drawPath.forEach(point => vertex(point.x, point.y))
+  //   endShape()
+  //   if(drawPath.length > 5) {
+  //     drawPath.shift()
+  //   }
+  // }
+}
 
 function modelLoaded() {
   console.log('model loaded (poseNet)')
@@ -72,10 +62,15 @@ function capturePose(poseData) {
   }
 }
 
-$.timer.addEventListener('click', event => startCountdown(event.target, 20))
-function startCountdown($timeDisplay, seconds){
-  countdown(seconds, $timeDisplay)
+function bodyPointTracking(bodyPoint, bodyCapture, strokePath) {
+  if (bodyCapture) {
+    fill(255, 255, 255)
+    ellipse(bodyCapture[bodyPoint].x + 320, bodyCapture[bodyPoint].y, 20)
+    strokePath.push(createVector(bodyCapture[bodyPoint].x + 320, bodyCapture[bodyPoint].y))
+  }
 }
+
+$.timer.addEventListener('click', event => countdown(event.target, 20))
 
 function countdown(counter, $timeDisplay) {
   count = setInterval(function() {
